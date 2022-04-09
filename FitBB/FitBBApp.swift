bf942340e36061909459456b8e9bedb1e032040b
Akbar <aumetov7@gmail.com>
2022-04-09 13:38:20 +0600

    Initial Commit

---
+++ FitBB/FitBBApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitBBApp.swift
+//  FitBB
+//
+//  Created by Акбар Уметов on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct FitBBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
