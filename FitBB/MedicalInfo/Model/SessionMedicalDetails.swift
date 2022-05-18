c48fbea82c92fdc23a4eb10e1abc0a861db1253a
aumetov7 <aumetov7@gmail.com>
2022-05-18 18:47:10 +0600

    Small refactoring

---
+++ FitBB/MedicalInfo/Model/SessionMedicalDetails.swift
@@ -0,0 +1,20 @@
+//
+//  SessionMedicalDetails.swift
+//  FitBB
+//
+//  Created by Акбар Уметов on 13/5/22.
+//
+
+import Foundation
+
+struct SessionMedicalDetails {
+    var weight: String
+    var height: String
+    var bloodPressure: String
+    var eyes: String
+    var spine: String
+    var spinePartSelectedIndex: Int
+    var heartSelectedIndex: Int
+    var jointsAndLigamentsSelectedIndex: Int
+}
+
--- FitBB/Model/SessionMedicalDetails.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SessionMedicalDetails.swift
-//  FitBB
-//
-//  Created by Акбар Уметов on 13/5/22.
-//
-
-import Foundation
